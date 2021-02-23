f506e239e9daaeba4beb3984f26605653f434c49
marieme <ps.marieme95@gmail.com>
2021-02-24 00:34:14 +0100

    Accès deploiement

--- app.cjs
+++ app.cjs
@@ -43,6 +43,7 @@ app.use(function (req, res, next) {
     res.setHeader('Access-Control-Allow-Origin', '*');
     res.setHeader('Access-Control-Allow-Origin', 'http://localhost:8080');
     res.setHeader('Access-Control-Allow-Origin', 'https://front-serveurdevoeux.herokuapp.com');
+    res.setHeader('Access-Control-Allow-Origin', 'http://serveurdevoeux.s3-website.eu-west-3.amazonaws.com');
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
     res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type,Accept, Authorization');
     res.setHeader('Access-Control-Allow-Credentials', true);
